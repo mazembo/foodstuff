1fe7814297b3778c0531d4e22cb3a1f5f10d945e
Mazembo <mazemb_eddy@yahoo.fr>
2014-01-30 12:10:58 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodstuff::Application.config.secret_key_base = '105cc95db84b4ca22cf6e65f0e6b61797a09d688850ce8a245927d2ab1367508acc532ee0b1631396b6fc50a49b8346a0e9681a41d43c3d2081ba2fa39c21e7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foodstuff::Application.config.session_store :cookie_store, key: '_foodstuff_session'
